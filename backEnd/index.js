1f37cd9e4c047e707defb68b4298c86d3e2a8a6b
ansadmk <ansadmk@gmail.com>
2024-03-06 21:21:41 +0530

    first commit

---
+++ backEnd/index.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+let addCount = 0;
+let updateCount = 0;
+app.post('/api/add', (req, res) => {
+    // Logic to add data
+    addCount++;
+    res.json({ message: 'Data added successfully' });
+});
+
+// API to edit data
+app.put('/api/edit/:id', (req, res) => {
+    // Logic to edit data
+    updateCount++;
+    res.json({ message: 'Data updated successfully' });
+});
+
+// API to get count
+app.get('/api/count', (req, res) => {
+    res.json({ addCount, updateCount });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Resizable } from "re-resizable";
+const App = () => {
+    // Define an array of context objects
+  const contexts = [
+    {
+      name: "Window-1",
+      sub: "component-1",
+      htmlContents: <>hi</>,
+    },
+    {
+      name: "Window-2",
+      sub: "component-2",
+      htmlContents: <>hi</>,
+    },
+    {
+      name: "Window-3",
+      sub: "component-3",
+      htmlContents: <>hi</>,
+    },
+  ];
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplate: "auto auto auto",
+        gridGap: "10px",
+        placeItems:"center",
+        width:"100vh"
+        
+      }}
+    >
+      {contexts.map(
+        (items: { name: String; sub: String; htmlContents: any }) => (
+          <Resizable
+            defaultSize={{
+              width: 320,
+              height: 200,
+            }}
+            style={{ border: "2px solid black",padding:'5rem',display:"flex",justifyContent:"center",alignItems:"center",flexDirection:'column' }}
+          >
+            <h1 style={{fontSize:"1.5rem"}}>{items.name}</h1>
+            <p>{items.sub}</p>
+            <div>{items.htmlContents}</div>
+          </Resizable>
+        )
+      )}
+    </div>
+  );
+};
+
+export default App;
